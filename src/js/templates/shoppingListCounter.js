1d94723d1473eaf65c70db6040f163fe31067649
YuriyMikh <pamukkale1717@gmail.com>
2023-06-17 23:09:23 +0300

    edit shoppingListCounter.js

--- src/js/templates/shoppingListCounter.js
+++ src/js/templates/shoppingListCounter.js
@@ -1,19 +1,36 @@
 import Storage from '../local-storage';
-const shoppingCountEls = document.querySelectorAll('.header-shopping-count');
+
+//-------Вячеслав:
+// const shoppingCountEls = document.querySelectorAll('.header-shopping-count');
+
+// export const countBook = () => {
+//   const storageBook = Storage.load('bookList');
+//   for (const shoppingCountEl of shoppingCountEls) {
+//     if (storageBook) {
+//       if (storageBook.length === 0) {
+//         shoppingCountEl.classList.add('is-hidden');
+//         return;
+//       }
+//       shoppingCountEl.classList.remove('is-hidden');
+//       shoppingCountEl.textContent = storageBook.length;
+//     } else {
+//       shoppingCountEl.classList.add('is-hidden');
+//     }
+//   }
+// };
+
+
+//----------Я:
+const shoppingCountEl = document.querySelector('.header-shopping-count');
 
 export const countBook = () => {
   const storageBook = Storage.load('bookList');
-  for (const shoppingCountEl of shoppingCountEls) {
-    if (storageBook) {
-      if (storageBook.length === 0) {
-        shoppingCountEl.classList.add('is-hidden');
-        return;
-      }
-      shoppingCountEl.classList.remove('is-hidden');
-      shoppingCountEl.textContent = storageBook.length;
-    } else {
-      shoppingCountEl.classList.add('is-hidden');
-    }
+
+  if (!storageBook || storageBook.length === 0) {
+    return
+  } else {
+    shoppingCountEl.classList.remove('is-hidden');
+    shoppingCountEl.textContent = storageBook.length;
   }
 };
 
